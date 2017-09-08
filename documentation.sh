ebce263e215559e660c3afbea96ebd55eb68630e
Justin Decker <justin000@gmail.com>
2017-09-08 15:13:52 -0700

    removed filed

--- documentation.sh
+++
@@ -1,9 +0,0 @@
-#doxygen Doxyfile
-#moxygen ./docs/xml/ --output api.md
-#rm ./docs/xml/ -R
-#cp api.md ../../doc/source/includes/
-cd ../../doc/
-bundle exec middleman build --clean
-cd build
-cp -R * ../../lib/pHProbe/docs/
-cd ../../lib/pHProbe/
